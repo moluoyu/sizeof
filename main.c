70726893ea3b3835779cca6140b5e3fa204e7b3c
luoyu <994252012@qq.com>
2018-03-29 18:03:51 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//#pragma pack(push)
+//#pragma  pack(4)
+struct  s
+{
+  //  union {
+        int a; //4
+       char str[10];//10
+
+ //   };
+    struct  s *next;//8
+
+} s;
+struct  s1
+{
+      union {
+    int a; //4
+    char str[10];//10
+
+      };
+    struct  s *next;//8
+
+} s1;
+struct s2{
+    char a;
+    int b;
+    double  c;
+
+} s2;
+int main() {
+    int *p;
+    printf("%d\n",sizeof(p));
+    printf("%d\n",sizeof(s));
+    printf("%d\n", sizeof(s1));
+    printf("%d\n", sizeof(s2));
+
+
+    return 0;
+}
